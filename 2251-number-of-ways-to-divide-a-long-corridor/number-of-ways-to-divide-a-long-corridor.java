2fb8a26b1a024562c6fd8fe875c06f592eb49194
subasree12-git <subasreesathish7@gmail.com>
2025-12-14 16:06:35 +0530

    Time: 13 ms (100.00%) | Memory: 47.5 MB (64.00%) - LeetSync

---
+++ 2251-number-of-ways-to-divide-a-long-corridor/number-of-ways-to-divide-a-long-corridor.java
@@ -0,0 +1,22 @@
+class Solution 
+{
+    public int numberOfWays(String corridor) 
+    {
+        final int n = corridor.length();
+        final byte[] corr = new byte[n];
+        corridor.getBytes(0, n, corr, 0);
+        long ways = 1;
+        int seatCount = 0;
+        int prevSeatIdx = -1;
+        for (int i = n - 1; i >= 0; i--) 
+        {
+            if (corr[i] == 'P')  continue;
+            if (seatCount != 0)
+                prevSeatIdx = i;
+            else if (prevSeatIdx > 0) 
+                ways = (ways * (prevSeatIdx - i)) % 1_000_000_007L;
+            seatCount ^= 1;
+        }
+        return (prevSeatIdx < 0 || seatCount != 0) ? 0 : (int)ways;
+    }
+}
