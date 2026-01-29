3fca6fed5175bdc4db3756e3e2448173d321b959
subasree12-git <subasreesathish7@gmail.com>
2026-01-29 12:41:04 +0530

    Time: 17 ms (6.01%) | Memory: 94 MB (95.90%) - LeetSync

---
+++ 234-palindrome-linked-list/palindrome-linked-list.java
@@ -0,0 +1,13 @@
+class Solution 
+{
+    public boolean isPalindrome(ListNode head) 
+    {
+        StringBuilder sb=new StringBuilder();
+        while(head!=null)
+        {
+            sb.append(head.val);
+            head=head.next;
+        }
+        return sb.toString().equals(sb.reverse().toString());
+    }
+}
